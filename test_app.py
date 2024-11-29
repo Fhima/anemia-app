233a1784249bf181115c1f6fb10c45f815ea4a2d
Fhima <Gabriel.fhima13@gmail.com>
2024-11-29 13:28:14 -0500

    Delete test_app.py

--- test_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title('Anemia Detection System - Test')
-st.write('This is a test deployment')
-
-uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
-if uploaded_file:
-    st.image(uploaded_file)
